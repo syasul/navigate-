c1053449bbb5cdce7b1f917e14759b8072aa16f3
syalsul <githubku4@gmail.com>
2021-03-19 22:46:02 +0700

    navigate

---
+++ app/src/main/java/com/example/login_navogate/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.login_navogate
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var btnIntent : Button
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnIntent = findViewById(R.id.btnIntent)
+        btnIntent.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when(v.id){
+            R.id.btnIntent ->{
+                val IntentBiasa = Intent(this@MainActivity, IntentBiasaActivity::class.java)
+                startActivity(IntentBiasa)
+            }
+        }
+    }
+
+}
